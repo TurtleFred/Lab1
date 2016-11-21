013ffeee24cd4726f0170e8069027ca3740ea53a
Fred <554214404@qq.com>
2016-11-21 20:15:16 +0800

    lab5b&w

---
+++ src/blackBox/blackBox.java
@@ -0,0 +1,54 @@
+package blackBox;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cn.edu.hit.exp1.Expression;
+import cn.edu.hit.exp1.Input;
+
+public class blackBox {
+    private Input ipt;
+    private Expression p;
+    
+    @Before
+    public void before(){
+        p = new Expression("2*a^3+4*b");
+    }
+    
+    @Test
+    public void test1(){
+        ipt = new Input("!d/d a");
+        ipt.derivative(p);
+        assertTrue(ipt.derivativeLegal());
+    }
+    
+    @Test
+    public void test2(){
+        ipt = new Input("!b/b a");
+        ipt.derivative(p);
+        assertFalse(ipt.derivativeLegal());
+    }
+    
+    @Test
+    public void test3(){
+        ipt = new Input("!d/d c");
+        ipt.derivative(p);
+        assertFalse(ipt.derivativeLegal());
+    }
+    
+    @Test
+    public void test4(){
+        ipt = new Input("!d/d a b");
+        ipt.derivative(p);
+        assertFalse(ipt.derivativeLegal());
+    }
+    
+    @Test
+    public void test5(){
+        ipt = new Input("!d/d");
+        ipt.derivative(p);
+        assertFalse(ipt.derivativeLegal());
+    }
+}
